0b5a319cc513a12f02ccb3c2ed99943ae5e8a9c2
jinhuachen1314 <jic212@ucsd.edu>
2019-05-02 17:50:37 -0700

    Create dynamic asset path to fonts

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Instagg
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
