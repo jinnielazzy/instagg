46ced480be38d7e14a93f0fde17cd32d24d25b64
jinhuachen1314 <jic212@ucsd.edu>
2019-05-04 13:14:16 -0700

    create posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :posts, only: [:index, :create, :update, :show, :destroy]
   end
 end
