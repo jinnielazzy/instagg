018ab9a0cac7c72281ecd990a28ba2e4ac5ed57d
jinhuachen1314 <jic212@ucsd.edu>
2020-04-23 22:54:07 -0700

    add profile hover

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -4,7 +4,23 @@ import { Link } from 'react-router-dom';
 function Post ({post}) {
   return (
     <div className="post-img">
-      <Link to={`/users/${post.user_id}`}><img src={post.img_url} /></Link>
+      <Link to={`/users/${post.user_id}`}>
+        <img src={post.img_url} />
+        <div className="item-info">
+          <ul>
+            <li className="item-likes">
+              <span className="visually-hidden">Likes:</span>
+              <i className="fas fa-heart" aria-hidden="true"></i>
+              56
+            </li>
+            <li className="item-comments">
+              <span className="visually-hidden">Comments:</span>
+              <i className="fas fa-comment" aria-hidden="true"></i>
+              34
+            </li>
+          </ul>
+        </div>
+      </Link>
     </div>
   )
 }
